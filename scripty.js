9d847c16a98a810186fe7f7797aee6de9200b376
bafag <arturabud13@gmail.com>
2026-01-17 00:32:27 -0300

    Update redirect URLs in verificar function

--- scripty.js
+++ scripty.js
@@ -2,13 +2,14 @@ function verificar() {
     let lg = document.getElementById("login").value;
     let sh = document.getElementById("senha").value;
     if (lg === "artur" && sh === "abud10022011") {
-        window.location.href = "http://127.0.0.1:5502/index.html";
+        window.location.href = "https://bafagofc.github.io/SiteDaEmpresa3/";
     }
     if (lg === "arruda" && sh === "filho201110") {
-        window.location.href = "http://127.0.0.1:5502/index.html";
+        window.location.href = "https://bafagofc.github.io/SiteDaEmpresa3/";
     }
      if (lg === "biel" && sh === "biel1212") {
-        window.location.href = "http://127.0.0.1:5502/index.html";
+        window.location.href = "https://bafagofc.github.io/SiteDaEmpresa3/";
     }
 
 }
+
